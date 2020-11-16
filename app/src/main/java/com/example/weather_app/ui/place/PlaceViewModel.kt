a6645b1e8cb5151678624f398dc4d4c993b23f45
Steven <55871540+DietCokewithSugar@users.noreply.github.com>
2020-11-16 21:36:21 +0800

    布局实现 以及应用图标等等

--- app/src/main/java/com/example/weather_app/ui/place/PlaceViewModel.kt
+++ app/src/main/java/com/example/weather_app/ui/place/PlaceViewModel.kt
@@ -1,4 +1,24 @@
 package com.example.weather_app.ui.place
 
-class PlaceViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.ViewModel
+import com.example.weather_app.logic.Repository
+import com.example.weather_app.logic.model.Place
+
+class PlaceViewModel : ViewModel(){
+
+    private val searchLiveData = MutableLiveData<String>()
+
+    val placeList = ArrayList<Place>()
+
+    val placeLiveData = Transformations.switchMap(searchLiveData){ query->
+        Repository.searchPlaces(query)
+    }
+
+    fun searchPlaces(query: String){
+        searchLiveData.value = query
+    }
+
+
 }
