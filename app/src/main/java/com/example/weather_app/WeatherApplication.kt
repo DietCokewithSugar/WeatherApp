cf49027c8c0da997495297fd565600d1289b95d7
Steven <55871540+DietCokewithSugar@users.noreply.github.com>
2020-11-18 12:34:06 +0800

    逻辑层实现完成, 可以搜索全球城市数据了

--- app/src/main/java/com/example/weather_app/WeatherApplication.kt
+++ app/src/main/java/com/example/weather_app/WeatherApplication.kt
@@ -23,4 +23,5 @@ class WeatherApplication : Application() {
     }
 
 
+
 }
--- app/src/main/java/com/example/weather_app/ui/place/PlaceViewModel.kt
+++ app/src/main/java/com/example/weather_app/ui/place/PlaceViewModel.kt
@@ -6,12 +6,20 @@ import androidx.lifecycle.ViewModel
 import com.example.weather_app.logic.Repository
 import com.example.weather_app.logic.model.Place
 
-class PlaceViewModel : ViewModel(){
+/**
+ * 链接逻辑层与ui界面
+ */
 
+class PlaceViewModel : ViewModel(){
+    /**
+     * 将传入的搜索的搜索参数赋值
+     */
     private val searchLiveData = MutableLiveData<String>()
 
+    //对界面上的显示城市的数据进行缓存,放在ViewModel类时,手机旋转不会丢失
     val placeList = ArrayList<Place>()
 
+    //使用switchMap观察这个对象，使用searchPlace()时,会自动转换为switchMap方法
     val placeLiveData = Transformations.switchMap(searchLiveData){ query->
         Repository.searchPlaces(query)
     }
